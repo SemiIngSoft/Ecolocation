ab000c245e8f5ad52e3c76420eb4e6a093641f90
merlinnusr <spike98@protonmail.com>
2018-10-08 21:43:14 -0500

    Usuarios ahora inserta en dos tablas

--- test_conection/usuario.php
+++ test_conection/usuario.php
@@ -3,9 +3,13 @@
     private $host = "localhost";
     private $dbname = "ecolocation";
     private $user = "root";
-    private $password = "";
+    private $password = "nano199699";
     private $conexion = null;
-    private $char = array(\PDO::MYSQL_ATTR_INIT_COMMAND =>  'SET NAMES utf8',\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    //private $char = array(\PDO::MYSQL_ATTR_INIT_COMMAND =>  'SET NAMES utf8',\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    private $options = array(\PDO::MYSQL_ATTR_INIT_COMMAND =>  'SET NAMES utf8',
+   						  PDO::ATTR_ERRMODE,
+   						  PDO::ERRMODE_EXCEPTION
+   						);
         
 
     public function getConexion(){
@@ -14,7 +18,7 @@
                   "mysql:host=$this->host; dbname=$this->dbname",
                   $this->user,
                   $this->password,
-                  $this->char
+                  $this->options
                   );
       return $this->conexion;
       }catch(Exception $e){
@@ -24,11 +28,15 @@
       }
     }
   }
+  	/** insercion a tabla de ciudadano */
+
+  	echo "*****Insercion a ciudadano ******";
+
+
 	$conexion = new Conexion();
 	$cnn = $conexion->getConexion();
 	$sql = "INSERT INTO ciudadano (nombre, apellidoPaterno,apellidoMaterno,ubicacion,domicilio,telefono,correo,password) VALUES('Mario','Jimenez','Mora','20.7049947,-103.32817690000002','Calle 7 Colinas 1772, Independencia, 44290 Guadalajara, Jal., Mexico','33-14-13-78-50','motorocool@gmail.com','$2y$12$1MfIIpZQ6HKjlhLMOqfyye.LY.5XqfNBaZAEiP.Ln7XlLGJB2Q.KO
-'); INSERT INTO ciudadano (nombre, apellidoPaterno,apellidoMaterno,ubicacion,domicilio,telefono,correo,password) VALUES('Pedro','Mariano','Mora','20.7049947,-103.32817690000002','Calle 8 Montes 1772, Independencia, 44290 Guadalajara, Jal., Mexico','33-17-13-77-57','pedo.mariano@gmail.com','$2y$12$1MfIIpZQ6HKjlhLMOqfyye.LY.5XqfNBaZAEiP.Ln7XlLGJB2Q.KO
-') ";
+');";
 
 	
 	$stmt = $cnn -> prepare($sql);
@@ -40,7 +48,9 @@
 		$row = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		foreach ($row as $ciudadano) {
 			echo "<pre>";
-			var_dump($ciudadano);
+			echo var_dump($ciudadano);
+
+			//var_dump();
 			echo "</pre>";
 		}
 		 /*while($row = $stmt -> fetchAll()){
@@ -49,6 +59,26 @@
 		 	/*echo "Id: ".$row["id"]." nombre: ".$row["nombre"]." apellido paterno ".$row["apellidoPaterno"]." Apellido materno ".$row["apellidoMaterno"]." ubicacion ".$row["ubicacion"]." domiciolio ". $row["domicilio"]. " telefono ". $row["telefono"]." correo ". $row["correo"]. " password ".$row["password"];*/
 		 /*	echo "</pre>";
 		 }*/
+	}
+	/**Insercion a tabla centro de centroderecoleccion*/
+	echo "*****Insercion a centro de recoleccion ******";
+	$conexion = new Conexion();
+	$cnn = $conexion->getConexion();
+	$sql = "INSERT INTO centroderecoleccion (nombre, ubicacion, domicilio) VALUES ('Centro Santa Anita','20.5502194,-103.44330560000003','Santa Anita #4444')";
+	$stmt = $cnn->prepare($sql);
+	$resultado = $stmt->execute();
+
+	if ($resultado) {
+		$sql = "SELECT * FROM centroderecoleccion ";
+		$stmt  = $cnn -> prepare ($sql);
+		$stmt -> execute();
+
+		$row = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+		foreach ($row as $centro) {
+			echo "<pre>";
+		 	echo var_dump($row);
+		 	echo "</pre>";
+		 } 
 	}
 	
 
